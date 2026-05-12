dc41f82ae455634f995d0275116e2bc0958e56cd
Adam Ottosson <addeecsgo@gmail.com>
2026-05-12 20:41:42 +0200

    Initial setup

---
+++ src/test/java/com/chatapp/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
