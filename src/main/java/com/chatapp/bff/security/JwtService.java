bea9ea70817de1a59afff569d746da4a0c3aed04
Adam Ottosson <addeecsgo@gmail.com>
2026-05-14 21:48:51 +0200

    Adds JwtService for token validation and username extraction

---
+++ src/main/java/com/chatapp/bff/security/JwtService.java
@@ -0,0 +1,35 @@
+package com.chatapp.bff.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+    private final Key key = Keys.hmacShaKeyFor(
+            "mysupersecretkeymysupersecretkey123456".getBytes()
+    );
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
